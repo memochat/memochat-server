5569db0494d8002204ec4897a6cea0b7941d4ed8
myeongil <username1103@naver.com>
2022-09-03 16:06:22 +0900

    test: RoomType엔티티 create, read 테스트

---
+++ src/app/memo-room/__test__/room-type.entity.spec.ts
@@ -0,0 +1,60 @@
+import { Test } from '@nestjs/testing';
+import { DataSource } from 'typeorm';
+import { DatabaseModule } from '../../../common/config/database/database.module';
+import { RoomType } from '../room-type.entity';
+import { MemoRoomCatrgory } from '../type/memo-room-category';
+
+describe('RoomType Entity Text', () => {
+  let dataSource: DataSource;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [DatabaseModule],
+    }).compile();
+
+    dataSource = module.get(DataSource);
+  });
+
+  afterEach(async () => {
+    await dataSource.destroy();
+  });
+
+  test('create', async () => {
+    // given
+    const roomType = new RoomType();
+    roomType.category = MemoRoomCatrgory.STUDY;
+    roomType.thumbnail = 'test.png';
+
+    const em = dataSource.createEntityManager();
+
+    // when
+    await em.save(roomType);
+
+    // then
+    expect(roomType).toMatchObject({
+      id: expect.any(Number),
+      category: MemoRoomCatrgory.STUDY,
+      thumbnail: 'test.png',
+    });
+  });
+
+  test('read', async () => {
+    // given
+    const roomType = new RoomType();
+    roomType.category = MemoRoomCatrgory.STUDY;
+    roomType.thumbnail = 'test.png';
+
+    const em = dataSource.createEntityManager();
+
+    // when
+    await em.save(roomType);
+
+    // then
+    const savedRoomType = await em.findOneBy(RoomType, { id: roomType.id });
+    expect(savedRoomType).toMatchObject({
+      id: expect.any(Number),
+      category: MemoRoomCatrgory.STUDY,
+      thumbnail: 'test.png',
+    });
+  });
+});
