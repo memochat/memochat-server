61bdf31c04c021bd4c76b0d350a05870eaf5c55a
hshs0409 <0409hshs@gmail.com>
2022-10-11 22:04:36 +0900

    refactor: :bug:

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, HttpStatus, Patch } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpStatus, Get, Patch } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ResponseEntity } from 'src/common/response/response-entity';
 import { ApiSuccessResponse } from '../../common/decorators/api-success-response.decorator';
 import { Auth } from '../../common/decorators/auth.decorator';
 import { CurrentUser } from '../../common/decorators/current-user.decorator';
@@ -11,6 +12,14 @@ import { UserService } from './user.service';
 @ApiTags('Users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('/me')
+  @Auth()
+  @ApiOperation({ summary: '유저 정보 조회 API', description: '로그인 된 유저의 정보를 조회합니다.' })
+  @ApiSuccessResponse(HttpStatus.OK)
+  async gets(@CurrentUser() user: User) {
+    return ResponseEntity.OK_WITH_DATA(user);
+  }
 
   @Patch('/nickname')
   @Auth()
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,36 +1,10 @@
-import { Injectable, HttpStatus, Get } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
-import { ApiSuccessResponse } from 'src/common/decorators/api-success-response.decorator';
-import { Auth } from 'src/common/decorators/auth.decorator';
-import { CurrentUser } from 'src/common/decorators/current-user.decorator';
-import { ResponseEntity } from 'src/common/response/response-entity';
+import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
-
-  /*
-  ## 생성  
-
-  userService에서 비밀번호 확인 및 비밀번호 변경 API 필요
-  GET /users/password 비밀번호 확인 API
-  PATCH /users/password 비밀번호 변경 API 
-
-  ## 수정
-  PATCH users/nickname 닉네임 변경 API
-  PATCH users/thumbnail 썸네일 변경 API
-
-  */
-
-  @Get('/me')
-  @Auth()
-  @ApiOperation({ summary: '유저 정보 조회 API', description: '로그인 된 유저의 정보를 조회합니다.' })
-  @ApiSuccessResponse(HttpStatus.OK)
-  async gets(@CurrentUser() user: User) {
-    return ResponseEntity.OK_WITH_DATA(user);
-  }
 
   async updateNickname(user: User, nickname: string) {
     user.updateNickname(nickname);
